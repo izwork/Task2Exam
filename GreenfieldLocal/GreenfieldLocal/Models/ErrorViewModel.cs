b22fe79f655c80f8d0ad9fb55c4ba1236aa356e7
izwork <iz.mail2608@gmail.com>
2026-04-13 11:54:28 +0100

    Created Project

---
+++ GreenfieldLocal/GreenfieldLocal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenfieldLocal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
